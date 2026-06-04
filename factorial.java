df3ea6b2d9912fb5a10c643913bdf633e9accbbb
Anuj Amol Musale <anujmusale2005@gmail.com>
2026-06-04 14:15:51 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    public static void main(String[] args) {
+        int number = 5; // Example number
+        long result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
